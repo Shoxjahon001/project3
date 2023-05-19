a104f1faea10ebb4feaef5f7b5fee8a0b51b9769
Shoxjahon001 <7967577@gmail.com>
2023-05-19 18:25:00 +0500

    something

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-const icon = document.querySelector(".fa-bars")
-const box = document.querySelector(".nav__2")
-
+const icon = document.querySelector(".fa-bars");
+const box = document.querySelector(".nav__2");
 icon.addEventListener("click", toggIeSlidebar);
-
 function toggIeSlidebar() {
   box.classList.toggle("nav__2__active");
 }
